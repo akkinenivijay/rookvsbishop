bbbe6e496f5a7be906bc8bc63af17666674101d3
Vijay Akkineni <akkineni.vijay@gmail.com>
2021-08-30 10:31:25 -0400

    renamed method

--- src/test/scala/com/zushealth/GameSuite.scala
+++ src/test/scala/com/zushealth/GameSuite.scala
@@ -7,7 +7,7 @@ class GameSuite extends AnyFunSuite {
     val rook = new Rook(1, 'h')
     val bishop = new Bishop(3, 'c')
     val board = new Board(rook, bishop)
-    val endResult = Game.recursiveGamePlay(rook, bishop, board, 0)
+    val endResult = Game.play(rook, bishop, board, 0)
     assert(endResult.contains("Bishop Captured Rook in iteration"))
   }
 }
